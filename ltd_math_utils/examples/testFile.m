76b9a1916f994a65f74081a763caa4ca86c4f943
armin schweitzer <armin.schweitzer@phys.ethz.ch>
2020-03-18 22:18:41 +0100

    Added ltd_tools.m, which has reworked algebra routines and implements spinors and polarization sums. Examples of new functionality are given in: ltd_math_utils/examples/examples.nb

---
+++ ltd_math_utils/examples/testFile.m
@@ -0,0 +1,163 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{-(g[mu1, mu2]*gamma[s6, muM3, s3]*gamma[s8, mu2, s5]*
+     Ttr[a[-1], a[-3], a[-2]]*(g[muM1, mu1]*(vector[p1, muM2] - 
+        vector[-p1 - p2, muM2]) + g[muM2, muM1]*(-vector[p1, mu1] + 
+        vector[p2, mu1]) + g[muM2, mu1]*(vector[-p1 - p2, muM1] - 
+        vector[p2, muM1]))*(Sqrt[Mt]*delta[s5, s6] + 
+      gamma[s5, mu56, s6]*vector[-k1 - p3, mu56])*(Mt*delta[s3, s8] + 
+      Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[-k1, mu34] + 
+      gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + delta[s4, s7]*
+         gamma[s3, mu34, s4]*vector[-k1, mu34])*vector[-k1 + p1 + p2 - p3, 
+        mu78]))/(2*s12) + (g[mu1, mu2]*gamma[s6, muM3, s3]*gamma[s8, mu2, s5]*
+    Ttr[a[-1], a[-2], a[-3]]*(g[muM1, mu1]*(vector[p1, muM2] - 
+       vector[-p1 - p2, muM2]) + g[muM2, muM1]*(-vector[p1, mu1] + 
+       vector[p2, mu1]) + g[muM2, mu1]*(vector[-p1 - p2, muM1] - 
+       vector[p2, muM1]))*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[-k1 - p3, mu56])*(Mt*delta[s3, s8] + 
+     Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[-k1, mu34] + 
+     gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+       delta[s4, s7]*gamma[s3, mu34, s4]*vector[-k1, mu34])*
+      vector[-k1 + p1 + p2 - p3, mu78]))/(2*s12), 
+ -(g[mu1, mu2]*gamma[s4, muM3, s5]*gamma[s6, mu2, s7]*
+     Ttr[a[-1], a[-3], a[-2]]*(g[muM1, mu1]*(vector[p1, muM2] - 
+        vector[-p1 - p2, muM2]) + g[muM2, muM1]*(-vector[p1, mu1] + 
+        vector[p2, mu1]) + g[muM2, mu1]*(vector[-p1 - p2, muM1] - 
+        vector[p2, muM1]))*(Sqrt[Mt]*delta[s5, s6] + 
+      gamma[s5, mu56, s6]*vector[k1 + p3, mu56])*(Mt*delta[s4, s7] + 
+      Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*vector[k1, mu34] + 
+      gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + delta[s3, s8]*
+         gamma[s3, mu34, s4]*vector[k1, mu34])*vector[k1 - p1 - p2 + p3, 
+        mu78]))/(2*s12) + (g[mu1, mu2]*gamma[s4, muM3, s5]*gamma[s6, mu2, s7]*
+    Ttr[a[-1], a[-2], a[-3]]*(g[muM1, mu1]*(vector[p1, muM2] - 
+       vector[-p1 - p2, muM2]) + g[muM2, muM1]*(-vector[p1, mu1] + 
+       vector[p2, mu1]) + g[muM2, mu1]*(vector[-p1 - p2, muM1] - 
+       vector[p2, muM1]))*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[k1 + p3, mu56])*(Mt*delta[s4, s7] + 
+     Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*vector[k1, mu34] + 
+     gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+       delta[s3, s8]*gamma[s3, mu34, s4]*vector[k1, mu34])*
+      vector[k1 - p1 - p2 + p3, mu78]))/(2*s12), 
+ (g[mu1, mu2]*gamma[s6, muM2, s3]*gamma[s8, mu2, s5]*Ttr[a[-1], a[-2], a[-3]]*
+    (Sqrt[Mt]*delta[s5, s6] + gamma[s5, mu56, s6]*vector[-k1 + p2, mu56])*
+    (Mt*delta[s3, s8] + Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*
+      vector[-k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+       delta[s4, s7]*gamma[s3, mu34, s4]*vector[-k1, mu34])*
+      vector[-k1 + p1 + p2 - p3, mu78])*
+    (g[muM3, muM1]*(-vector[p1, mu1] + vector[-p3, mu1]) + 
+     g[muM3, mu1]*(-vector[-p3, muM1] + vector[-p1 + p3, muM1]) + 
+     g[muM1, mu1]*(vector[p1, muM3] - vector[-p1 + p3, muM3])))/(2*s13) + 
+  (g[mu1, mu2]*gamma[s6, muM2, s3]*gamma[s8, mu2, s5]*
+    Ttr[a[-1], a[-3], a[-2]]*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[-k1 + p2, mu56])*(Mt*delta[s3, s8] + 
+     Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[-k1, mu34] + 
+     gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+       delta[s4, s7]*gamma[s3, mu34, s4]*vector[-k1, mu34])*
+      vector[-k1 + p1 + p2 - p3, mu78])*
+    (g[muM3, muM1]*(vector[p1, mu1] - vector[-p3, mu1]) + 
+     g[muM3, mu1]*(vector[-p3, muM1] - vector[-p1 + p3, muM1]) + 
+     g[muM1, mu1]*(-vector[p1, muM3] + vector[-p1 + p3, muM3])))/(2*s13), 
+ (g[mu1, mu2]*gamma[s4, muM2, s5]*gamma[s6, mu2, s7]*Ttr[a[-1], a[-2], a[-3]]*
+    (Sqrt[Mt]*delta[s5, s6] + gamma[s5, mu56, s6]*vector[k1 - p2, mu56])*
+    (g[muM3, muM1]*(-vector[p1, mu1] + vector[-p3, mu1]) + 
+     g[muM3, mu1]*(-vector[-p3, muM1] + vector[-p1 + p3, muM1]) + 
+     g[muM1, mu1]*(vector[p1, muM3] - vector[-p1 + p3, muM3]))*
+    (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+      vector[k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+       delta[s3, s8]*gamma[s3, mu34, s4]*vector[k1, mu34])*
+      vector[k1 - p1 - p2 + p3, mu78]))/(2*s13) + 
+  (g[mu1, mu2]*gamma[s4, muM2, s5]*gamma[s6, mu2, s7]*
+    Ttr[a[-1], a[-3], a[-2]]*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[k1 - p2, mu56])*
+    (g[muM3, muM1]*(vector[p1, mu1] - vector[-p3, mu1]) + 
+     g[muM3, mu1]*(vector[-p3, muM1] - vector[-p1 + p3, muM1]) + 
+     g[muM1, mu1]*(-vector[p1, muM3] + vector[-p1 + p3, muM3]))*
+    (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+      vector[k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+       delta[s3, s8]*gamma[s3, mu34, s4]*vector[k1, mu34])*
+      vector[k1 - p1 - p2 + p3, mu78]))/(2*s13), 
+ (g[mu1, mu2]*gamma[s6, muM1, s3]*gamma[s8, mu2, s5]*Ttr[a[-1], a[-3], a[-2]]*
+    (Sqrt[Mt]*delta[s5, s6] + gamma[s5, mu56, s6]*vector[-k1 + p1, mu56])*
+    (Mt*delta[s3, s8] + Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*
+      vector[-k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+       delta[s4, s7]*gamma[s3, mu34, s4]*vector[-k1, mu34])*
+      vector[-k1 + p1 + p2 - p3, mu78])*
+    (g[muM2, muM3]*(-vector[p2, mu1] + vector[-p3, mu1]) + 
+     g[muM3, mu1]*(-vector[-p3, muM2] + vector[-p2 + p3, muM2]) + 
+     g[muM2, mu1]*(vector[p2, muM3] - vector[-p2 + p3, muM3])))/(2*s23) + 
+  (g[mu1, mu2]*gamma[s6, muM1, s3]*gamma[s8, mu2, s5]*
+    Ttr[a[-1], a[-2], a[-3]]*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[-k1 + p1, mu56])*(Mt*delta[s3, s8] + 
+     Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[-k1, mu34] + 
+     gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+       delta[s4, s7]*gamma[s3, mu34, s4]*vector[-k1, mu34])*
+      vector[-k1 + p1 + p2 - p3, mu78])*
+    (g[muM2, muM3]*(vector[p2, mu1] - vector[-p3, mu1]) + 
+     g[muM3, mu1]*(vector[-p3, muM2] - vector[-p2 + p3, muM2]) + 
+     g[muM2, mu1]*(-vector[p2, muM3] + vector[-p2 + p3, muM3])))/(2*s23), 
+ (g[mu1, mu2]*gamma[s4, muM1, s5]*gamma[s6, mu2, s7]*Ttr[a[-1], a[-3], a[-2]]*
+    (Sqrt[Mt]*delta[s5, s6] + gamma[s5, mu56, s6]*vector[k1 - p1, mu56])*
+    (g[muM2, muM3]*(-vector[p2, mu1] + vector[-p3, mu1]) + 
+     g[muM3, mu1]*(-vector[-p3, muM2] + vector[-p2 + p3, muM2]) + 
+     g[muM2, mu1]*(vector[p2, muM3] - vector[-p2 + p3, muM3]))*
+    (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+      vector[k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+       delta[s3, s8]*gamma[s3, mu34, s4]*vector[k1, mu34])*
+      vector[k1 - p1 - p2 + p3, mu78]))/(2*s23) + 
+  (g[mu1, mu2]*gamma[s4, muM1, s5]*gamma[s6, mu2, s7]*
+    Ttr[a[-1], a[-2], a[-3]]*(Sqrt[Mt]*delta[s5, s6] + 
+     gamma[s5, mu56, s6]*vector[k1 - p1, mu56])*
+    (g[muM2, muM3]*(vector[p2, mu1] - vector[-p3, mu1]) + 
+     g[muM3, mu1]*(vector[-p3, muM2] - vector[-p2 + p3, muM2]) + 
+     g[muM2, mu1]*(-vector[p2, muM3] + vector[-p2 + p3, muM3]))*
+    (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+      vector[k1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+       delta[s3, s8]*gamma[s3, mu34, s4]*vector[k1, mu34])*
+      vector[k1 - p1 - p2 + p3, mu78]))/(2*s23), 
+ (gamma[s2, muM2, s5]*gamma[s4, muM1, s1]*gamma[s8, muM3, s3]*
+   Ttr[a[-1], a[-3], a[-2]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[-k1, mu12])*(Sqrt[Mt]*delta[s3, s4] + 
+    gamma[s3, mu34, s4]*vector[-k1 + p1, mu34])*
+   (Mt*delta[s5, s8] + Sqrt[Mt]*delta[s6, s8]*gamma[s5, mu56, s6]*
+     vector[-k1 - p2, mu56] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s5, s7] + 
+      delta[s6, s7]*gamma[s5, mu56, s6]*vector[-k1 - p2, mu56])*
+     vector[-k1 + p1 - p3, mu78]))/2, 
+ (gamma[s2, muM1, s3]*gamma[s4, muM3, s7]*gamma[s6, muM2, s1]*
+   Ttr[a[-1], a[-2], a[-3]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[k1, mu12])*(Sqrt[Mt]*delta[s3, s4] + 
+    gamma[s3, mu34, s4]*vector[k1 - p1, mu34])*(Mt*delta[s6, s7] + 
+    Sqrt[Mt]*delta[s5, s7]*gamma[s5, mu56, s6]*vector[k1 + p2, mu56] + 
+    gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s6, s8] + 
+      delta[s5, s8]*gamma[s5, mu56, s6]*vector[k1 + p2, mu56])*
+     vector[k1 - p1 + p3, mu78]))/2, 
+ (gamma[s2, muM2, s5]*gamma[s4, muM1, s1]*gamma[s6, muM3, s7]*
+   Ttr[a[-1], a[-3], a[-2]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[-k1, mu12])*(Sqrt[Mt]*delta[s5, s6] + 
+    gamma[s5, mu56, s6]*vector[-k1 - p2, mu56])*
+   (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+     vector[-k1 + p1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+      delta[s3, s8]*gamma[s3, mu34, s4]*vector[-k1 + p1, mu34])*
+     vector[-k1 - p2 + p3, mu78]))/2, 
+ (gamma[s2, muM1, s3]*gamma[s6, muM2, s1]*gamma[s8, muM3, s5]*
+   Ttr[a[-1], a[-2], a[-3]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[k1, mu12])*(Sqrt[Mt]*delta[s5, s6] + 
+    gamma[s5, mu56, s6]*vector[k1 + p2, mu56])*(Mt*delta[s3, s8] + 
+    Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[k1 - p1, mu34] + 
+    gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+      delta[s4, s7]*gamma[s3, mu34, s4]*vector[k1 - p1, mu34])*
+     vector[k1 + p2 - p3, mu78]))/2, 
+ (gamma[s2, muM3, s5]*gamma[s4, muM1, s1]*gamma[s6, muM2, s7]*
+   Ttr[a[-1], a[-2], a[-3]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[-k1, mu12])*(Sqrt[Mt]*delta[s5, s6] + 
+    gamma[s5, mu56, s6]*vector[-k1 + p3, mu56])*
+   (Mt*delta[s4, s7] + Sqrt[Mt]*delta[s3, s7]*gamma[s3, mu34, s4]*
+     vector[-k1 + p1, mu34] + gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s4, s8] + 
+      delta[s3, s8]*gamma[s3, mu34, s4]*vector[-k1 + p1, mu34])*
+     vector[-k1 - p2 + p3, mu78]))/2, 
+ (gamma[s2, muM1, s3]*gamma[s6, muM3, s1]*gamma[s8, muM2, s5]*
+   Ttr[a[-1], a[-3], a[-2]]*(Sqrt[Mt]*delta[s1, s2] + 
+    gamma[s1, mu12, s2]*vector[k1, mu12])*(Sqrt[Mt]*delta[s5, s6] + 
+    gamma[s5, mu56, s6]*vector[k1 - p3, mu56])*(Mt*delta[s3, s8] + 
+    Sqrt[Mt]*delta[s4, s8]*gamma[s3, mu34, s4]*vector[k1 - p1, mu34] + 
+    gamma[s7, mu78, s8]*(Sqrt[Mt]*delta[s3, s7] + 
+      delta[s4, s7]*gamma[s3, mu34, s4]*vector[k1 - p1, mu34])*
+     vector[k1 + p2 - p3, mu78]))/2}
