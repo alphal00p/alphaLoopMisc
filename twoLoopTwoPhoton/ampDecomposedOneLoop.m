3fd315a63c86323f3e1cbd2044854ca1576c9b14
Mao Zeng <zengmao@hotmail.co.uk>
2020-06-30 14:07:34 +0200

    Added comments to ampDecomposedOneLoop.m.

--- twoLoopTwoPhoton/ampDecomposedOneLoop.m
+++ twoLoopTwoPhoton/ampDecomposedOneLoop.m
@@ -1,3 +1,5 @@
+(* The finite subtracted one-loop integrand. The numerical value of p1 must be proportional to (1,0,0,1), and the numerical value of p2 must be proportional to (1,0,0,-1).
+p1 and p2 are the incoming electron / positron momenta. p3 and p4 are the outgoing off-shell photon momenta, and the corresponding photon polarization vectors are written as p5 and p6. We must have p1+p2=p3+p4. k1 is the loop momentum.*)
 {(-I)*d[mu1, mu2]*d[mu13, mu14]*gamma[tree, p2, mu14, k1 - p2, p6, 
    k1 + p1 - p3, p5, k1 + p1, mu13, p1]*propagator[k1]*propagator[k1 + p1]*
   propagator[k1 - p2]*propagator[k1 + p1 - p3], 
