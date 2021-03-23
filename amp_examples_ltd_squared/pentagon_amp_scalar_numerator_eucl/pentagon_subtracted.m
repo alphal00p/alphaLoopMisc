5a4b3bd5c116c7ffbd156b4b16a72bb220cedbbf
armin schweitzer <armin.schweitzer@phys.ethz.ch>
2021-03-23 08:02:07 +0100

    added runcards for comparison of hardcoded constants vs rational functions in numerator

--- amp_examples_ltd_squared/pentagon_amp_scalar_numerator_eucl/pentagon_subtracted.m
+++ amp_examples_ltd_squared/pentagon_amp_scalar_numerator_eucl/pentagon_subtracted.m
@@ -16,16 +16,16 @@ a10->1/((m1-2 p1.q1) (m1+m2-2 p1.q1-2 p1.q2+2 q1.q2))
 
 
 
-rr = {p1 -> (Rationalize[#,0]&/@{1, 0, 0, 1}),
-p2 -> (Rationalize[#,0]&/@{-1, 0., 0., 1.}),
-q1 -> (Rationalize[#,0]&/@{0.,2.3,0.2,1.1}),
-q2 -> (Rationalize[#,0]&/@{0.,0.2,1.3,3.123})};
+rr = {p1 -> (Rationalize[#,10^-32]&/@{1, 0, 0, 1}),
+p2 -> (Rationalize[#,10^-32]&/@{-1, 0., 0., 1.}),
+q1 -> (Rationalize[#,10^-32]&/@{0.,2.3,0.2,1.1}),
+q2 -> (Rationalize[#,10^-32]&/@{0.,0.2,1.3,3.123})};
+
+
+rr //N
 
 
 andreaNum=(andrea /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3]) /. rr /. sp[a_,b_]:>a[[1]]*b[[1]] - Dot[a[[2;;]],b[[2;;]]] //N
-
-
-andreaNumEucl=(andrea /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3]) /. rr2 /. sp[a_,b_]:>a[[1]]*b[[1]] - Dot[a[[2;;]],b[[2;;]]] //N
 
 
 rrZenoToAndrea =
@@ -37,7 +37,7 @@ Sort@(Rule[-Values[#],-Keys[#]]&/@{ a3 -> -xZenoConst4,
 })
 
 
-rrZenoToAndrea /. andreaNumEucl
+rrZenoToAndrea /. andreaNum // N[#,20]&
 
 
 numerator = 1-xZenoConst1*(k1.k1+2*k1.p1+p1.p1)*(k1.k1+k2.k2+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1)+xZenoConst2*(k1.k1+2*k1.p1+p1.p1)*(k1.k1+k2.k2+k3.k3+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1-2*k3.k1+2*k3.k2-2*k3.p1)+xZenoConst3*(k1.k1+k2.k2+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1)*(k1.k1+k2.k2+k3.k3+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1-2*k3.k1+2*k3.k2-2*k3.p1)+xZenoConst4*(k1.k1+k2.k2+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1)*(k1.k1-2*k1.p2+p2.p2)+xZenoConst5*(k1.k1+k2.k2+k3.k3+p1.p1+2*k1.p1-2*k1.k2-2*k2.p1-2*k3.k1+2*k3.k2-2*k3.p1)*(k1.k1-2*k1.p2+p2.p2);
@@ -46,13 +46,16 @@ numerator = 1-xZenoConst1*(k1.k1+2*k1.p1+p1.p1)*(k1.k1+k2.k2+p1.p1+2*k1.p1-2*k1.
 StringReplace[#,{"["->"(","]"->")"}]&@(ToString[(numerator /. Dot -> sp),InputForm])
 
 
+setTo0 ={1->1};(* {xZenoConst1 ->0, xZenoConst2 ->0, xZenoConst4->0,xZenoConst5 ->0} *);
+
+
 test0 = numerator /. setTo0/. rrZenoToAndrea /. andrea  /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3] /. q1->p3 /. q2->p4 /. q3 ->p5 /. k3 ->p4 /. k2 -> p3//Simplify;
 
 
-setTo0 = {xZenoConst1 ->0, xZenoConst2 ->0, xZenoConst4->0,xZenoConst5 ->0};
+test1 = StringReplace[#,{"["->"(","]"->")"}]&@(ToString[#,InputForm]&@(numerator/. setTo0 /. rrZenoToAndrea /. andrea /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3] /. q1->p3 /. q2->p4 /. q3 ->p5 /. k3 ->p4 /. k2 -> p3//Simplify)) ;
 
 
-test1 = StringReplace[#,{"["->"(","]"->")"}]&@(ToString[#,InputForm]&@(numerator-1 /. setTo0 /. rrZenoToAndrea /. andrea /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3] /. q1->p3 /. q2->p4 /. q3 ->p5 /. k3 ->p4 /. k2 -> p3//Simplify)) ;
+test1
 
 
 test1b = StringReplace[#,{"["->"(","]"->")"}]&@(ToString[#,InputForm]&@(numerator-1 /. setTo0   /. Dot->sp /. m1 ->sp[q1,q1] /. m2 -> sp[q2,q2] /. m3 -> sp[q3,q3] /. q1->p3 /. q2->p4 /. q3 ->p5 /. k3 ->p4 /. k2 -> p3//Simplify)) ;
