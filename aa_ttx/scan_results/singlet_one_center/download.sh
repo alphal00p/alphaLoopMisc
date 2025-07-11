c85c8664b0ed9549ae8d651974b7ebe9379b0d5c
ValentinHirschi <valentin.hirschi@gmail.com>
2025-07-11 11:47:38 +0200

    Update singlet results and loop-induced results

--- aa_ttx/scan_results/singlet_one_center/download.sh
+++ aa_ttx/scan_results/singlet_one_center/download.sh
@@ -1,1 +1,1 @@
-scp itphlies_tunnel:"/shared/zeno/aa_ttx/runs/NNLO_from_gL__ttbar_singlets_one_source_runs/run_workspace/run_*/nnlo_all_singlets_*_sqrts_*_results.txt" .
+scp itphlies_tunnel:"/shared/zeno/aa_ttx/runs/NNLO_from_gL__ttbar_singlets_one_center_runs/run_workspace/run_*/nnlo_all_singlets_*_sqrts_*_results.txt" .
