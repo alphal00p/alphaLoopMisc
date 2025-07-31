78e0778973f443c1f2ab3a66814b5471a0798fb2
ValentinHirschi <valentin.hirschi@gmail.com>
2025-07-31 09:27:07 +0200

    Final update of singlet results

--- aa_ttx/scan_results/singlet_one_center_deep/download.sh
+++ aa_ttx/scan_results/singlet_one_center_deep/download.sh
@@ -1,1 +1,2 @@
 scp itphlies_tunnel:"/shared/zeno/aa_ttx/runs/NNLO_from_gL__ttbar_singlets_one_center_runs/run_workspace/run_1337*/nnlo_all_singlets_*_sqrts_*_results.txt" .
+rm nnlo_all_singlets_13_sqrts_390_0_results.txt nnlo_all_singlets_11_sqrts_362_0_results.txt
