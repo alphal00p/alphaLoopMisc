474f4d24df32a02b5020af0227b74fd79d25d897
ValentinHirschi <valentin.hirschi@gmail.com>
2025-11-11 20:41:41 +0100

    Removed the floating-point hack since now symbolica parses them correctly.

--- SymbolicaCommunityBridge/SCBPython/utilities.py
+++ SymbolicaCommunityBridge/SCBPython/utilities.py
@@ -4,16 +4,15 @@ except ImportError:
     # User may not have selected symbolica features to be available
     pass
 
-import re
-
-floating_point_re = re.compile(r'((?:\d+(?:\.\d*)?|\.\d+)(?:`\d*)?)[eE]([+-]?\d+)')
+#import re
+#floating_point_re = re.compile(r'((?:\d+(?:\.\d*)?|\.\d+)(?:`\d*)?)[eE]([+-]?\d+)')
 
 
 def to_symbolica(expr: str, debug=False) -> Expression:
     if debug:
         print("Resulting string form of expression from Mathematica:\n%s"%expr)
     input_str = expr.replace(
-        'MATHEMATICABACKTICK', '`').replace('"', 'MATHEMATICADOUBLEQUOTES').replace('*^','e')
+        'MATHEMATICABACKTICK', '`').replace('"', 'MATHEMATICADOUBLEQUOTES')#.replace('*^','e')
     if debug:
         print("Expression sent for parsing with Symboolica:\n%s"%input_str)
     e = E(input_str, ParseMode.Mathematica)
@@ -24,4 +23,5 @@ def to_symbolica(expr: str, debug=False) -> Expression:
 
 def to_mathematica_form(expr: str) -> str:
     processed_expr = expr.replace('MATHEMATICADOUBLEQUOTES', '"')
-    return re.sub(floating_point_re, r'\1*^\2',  processed_expr)
+    #return re.sub(floating_point_re, r'\1*^\2',  processed_expr)
+    return processed_expr
